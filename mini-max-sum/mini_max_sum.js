4d2dce2dd096de4307543730196364ddb9c3a208
Flu3nt <gnedyalkov94@gmail.com>
2022-05-17 17:53:15 +0300

    Created a second simpler solution

--- mini-max-sum/mini_max_sum.js
+++ mini-max-sum/mini_max_sum.js
@@ -25,3 +25,23 @@ function miniMax(arr) {
 
 miniMax([1, 3, 5, 7, 9]);
 miniMax([7, 69, 2, 221, 8974]);
+
+
+function miniMaxTwo(numberArray) {
+
+    numberArray.sort((a, b) => a - b);
+    let maxSum = 0;
+    let minSum = 0;
+
+    for (let i = 1; i < numberArray.length; i++) {
+        maxSum += numberArray[i];
+    }
+
+    for (let i = 0; i < numberArray.length - 1; i++) {
+        minSum += numberArray[i];
+    }
+    console.log(`${minSum} ${maxSum}`);
+}
+
+miniMaxTwo([1, 3, 5, 7, 9]);
+miniMaxTwo([7, 69, 2, 221, 8974]);
