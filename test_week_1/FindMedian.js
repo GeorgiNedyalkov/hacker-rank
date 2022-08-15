6917feed3457d747ed53a729f678d471aadef1fe
Flu3nt <gnedyalkov94@gmail.com>
2022-08-15 15:22:40 +0300

    Solved the problem for odds list

--- test_week_1/FindMedian.js
+++ test_week_1/FindMedian.js
@@ -1,10 +1,9 @@
 function findMedianInOddsList(arr) {
   arr.sort();
   const medianIndex = Math.floor(arr.length / 2);
-
-  const nums = [...arr];
-
   console.log(nums[medianIndex]);
 }
 
 findMedianInOddsList([0, 1, 2, 13, 6, 5, 3]);
+
+function findMedianInEvenList(arr) {}
