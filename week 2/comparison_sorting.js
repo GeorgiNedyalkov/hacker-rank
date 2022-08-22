13a156afa36fc6d8cf2a319cee10fc964050acdb
Flu3nt <gnedyalkov94@gmail.com>
2022-08-22 18:28:45 +0300

    Started solving the counting sort problem

---
+++ week 2/comparison_sorting.js
@@ -0,0 +1,19 @@
+function comparisonSorting(arr) {
+  let result = [];
+
+  while (result.length <= arr.length) {
+    result.push(0);
+  }
+
+  //   for (let i = 0; i < arr.length; i++) {
+  //     if (result.includes(arr[i])) {
+  //       result[arr[i]]++;
+  //     } else {
+  //       result.findIndex();
+  //     }
+  //   }
+
+  console.log(result);
+}
+
+comparisonSorting([1, 1, 3, 2, 1]);
